911259211af4e8f7527180b1f19fddc7f39425ce
Björn Gåvefeldt <swizzy@xeupd.com>
2025-07-26 11:06:30 +0200

    Added a Font preview for the Font selector

---
+++ Cronus Zen Message Screen Studio/FontFamilyConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CronusZenMessageScreenStudio;
+
+public class FontFamilyConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is System.Drawing.FontFamily drawingFontFamily)
+        {
+            return new FontFamily(drawingFontFamily.Name);
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
